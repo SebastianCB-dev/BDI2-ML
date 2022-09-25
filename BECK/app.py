d883a730b6c02025e3b6977427e1933ea22965ca
SebastiánCB <sebastian.carrillob01@gmail.com>
2022-09-24 22:11:19 -0500

    Feat: Added word2vec.model

--- BECK/app.py
+++ BECK/app.py
@@ -1,38 +1,25 @@
-import json
-import pandas as pd
-from preprocessing_service import preprocesamiento
-from embeddings import word_space
-from preprocessing_service import preprocesamiento_no_ortografia
-from file_service import read_json, read_txt
-import gensim as gensim
-from pprint import pprint
-# Leer información del archivo items.json y de comentariostest
-beck_data = json.loads(read_json('./JSON/items.json'))
-comments_array = list(read_txt('./comentarios_test.txt'))
+# import json
+# import pandas as pd
+# from preprocessing_service import preprocesamiento
+# from embeddings import word_space
+# from preprocessing_service import preprocesamiento_no_ortografia
+# from file_service import read_json, read_txt
+from gensim.models import Word2Vec
+# from pprint import pprint
+# # Leer información del archivo items.json y de comentariostest
+# beck_data = json.loads(read_json('./JSON/items.json'))
+# comments_array = list(read_txt('./comentarios_test.txt'))
 
-# Arreglo de las 21 categorias del BDI-II
-types = list(beck_data.keys())
+# # Arreglo de las 21 categorias del BDI-II
+# types = list(beck_data.keys())
 
-beck_data_preprocessing = {}
-try:
-  if open('./JSON/items_preprocessing.json', 'r'):
-    beck_data_preprocessing = json.loads(read_json('./JSON/items_preprocessing.json'))
-except Exception as e:
-  print(f'Error: {e}')  
+# beck_data_preprocessing = {}
+# try:
+#   if open('./JSON/items_preprocessing.json', 'r'):
+#     beck_data_preprocessing = json.loads(read_json('./JSON/items_preprocessing.json'))
+# except Exception as e:
+#   print(f'Error: {e}')  
 
-pprint(beck_data_preprocessing)    
+# pprint(beck_data_preprocessing)    
 
 # !Vector Space Embedding
-
-dictionary = gensim.corpora.dictionary.Dictionary.load('./myModel.sav')
-comment = 'a'
-while (comment != '0'):
-  comment = input('Ingrese el texto: ')
-  comment_preprocessing_input = preprocesamiento(comment)
-  result = dictionary.doc2bow(comment_preprocessing_input)
-  if(len(result) == 0):
-    # Entrenar la red neuronal con el nuevo texto
-    pass
-  
-  print('Array:', comment_preprocessing_input)
-  print('El vector es:', result)
