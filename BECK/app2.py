8c8c01a2b3622444fa326f28ca1befd604a9f89a
Juan David Mejia Morales <2000jdjuandavid@gmail.com>
2022-09-26 19:34:33 -0500

    Doont delete

--- BECK/app2.py
+++ BECK/app2.py
@@ -3,33 +3,47 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import euclidean_distances
-import numpy as np
+import operator
+#txt call with open tool
 comentarios= list(open('comentarios_test.txt', 'r', encoding='utf-8').readlines())
 beck_data_preprocessing = {}
+#json with preprocessing items
 try:
   if open('./JSON/items_preprocessing.json', 'r'):
     beck_data_preprocessing = json.loads(open('./JSON/items_preprocessing.json', 'r', encoding='utf-8').read())
 except Exception as e:
   print(f'Error: {e}')
-
+#first comment call is necesary a coma
 lista= comentarios[0],
 print(lista)
-
+#route in beck dict and key(Tristeza call)
 for key in beck_data_preprocessing.keys():
-    for llave in beck_data_preprocessing["Tristeza"].values():
-        coment_compar= llave["data"]
+    #Tristeza values
+    for items in beck_data_preprocessing["Tristeza"].values():
+        coment_compar= items["data"]
         print(coment_compar)
-        valor = llave["value"]
+        valor = items["value"]
         vectorizer = CountVectorizer()
-        features = vectorizer.fit_transform(lista).todense()
-        features2 = vectorizer.fit_transform(coment_compar).todense()
-        print(features)
-        print(features2)
-        print(euclidean_distances(features[0], features2[0]))
+        #This method performs fit and transform on the input data at a single time and converts the data points.
+        insta_coment = vectorizer.fit_transform(lista).todense()
+        feature = vectorizer.fit_transform(coment_compar).todense()
+        beck_coment = np.concatenate((feature), axis=1)
+        diferencia = -(insta_coment.size - beck_coment.size)
+        print(diferencia)
+        if(insta_coment.size<beck_coment.size):
+            [*lista]
+        print(type(lista))
+
+        print(insta_coment.size)
+        print(beck_coment.size)
+
+
+
+        print("Comentario insta",insta_coment[0])
+        print("Comentario Beck ",beck_coment[0])
+
+
         #print(vectorizer.vocabulary_)
-        for f in features:
-            pass
-            #print("fff",f)
-            #print("ggg",features[0])
-            #print( euclidean_distances(features[0], f) )
-        # print(euclidean_distances(comentarios1[0],f))
+        for beck in  insta_coment:
+            print(euclidean_distances(beck, beck_coment[0]))
+
