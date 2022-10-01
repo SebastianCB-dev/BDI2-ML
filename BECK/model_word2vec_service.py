1926163003cb4d1c00cd6b629467e9cd5b8fbb77
Joan Sebastián Carrillo Barón <sebastian.carrillob01@gmail.com>
2022-10-01 08:51:23 -0500

    Feat: Added Word2Vec Service

---
+++ BECK/model_word2vec_service.py
@@ -0,0 +1,23 @@
+from gensim.models import Word2Vec
+import numpy as np
+
+class ModelWord2Vec:
+
+  def __init__(self):
+    self.model = Word2Vec.load('word2vec.model')
+
+  def add_corpus(self, corpus: list[str]):
+    self.model.build_vocab(corpus)
+    self.save_model()
+
+  def save_model(self):
+    self.model.save('word2vec.model')
+
+  def get_euclidian_distance(self, corpus_a, corpus_b):
+    return np.linalg.norm(self.model.wv[corpus_a] - self.model.wv[corpus_b])
+
+  def get_cosine_distance(self, corpus_a, corpus_b):
+    return self.model.wv.wmdistance(corpus_a, corpus_b)
+
+  def get_word_vector(self, word):
+    return self.model.wv[word]
