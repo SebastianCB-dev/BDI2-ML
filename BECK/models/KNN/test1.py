d46ede9f64e7012ed4307262a7e86b7ef59c12c6
SebastianCB <sebastian.carrillob01@gmail.com>
2022-10-29 20:30:16 -0500

    Feat: Created models folder

--- BECK/models/KNN/test1.py
+++ BECK/models/KNN/test1.py
@@ -5,7 +5,7 @@ from sklearn.metrics import classification_report, confusion_matrix, f1_score
 def train_val_test_split(df, rstate=42, shuffle=True, stratify=None):
     strat = df[stratify] if stratify else None
     train_set, test_set = train_test_split(
-        df, test_size=0.3, random_state=rstate, shuffle=shuffle, stratify=strat)
+        df, test_size=0.25, random_state=rstate, shuffle=shuffle, stratify=strat)
     strat = test_set[stratify] if stratify else None
     val_set, test_set = train_test_split(
         test_set, test_size=0.5, random_state=rstate, shuffle=shuffle, stratify=strat)
