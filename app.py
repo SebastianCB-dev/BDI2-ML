e9a6a15560e1c42ff2afe114d074daa4f60d7b5c
SebastianCB <sebastian.carrillob01@gmail.com>
2022-12-03 19:22:53 -0500

    Feat:Added model LR

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from preprocessing_service import Preprocesamiento
 from model_word2vec_service import ModelWord2Vec
+import joblib
 
 pp = Preprocesamiento()
 w2v = ModelWord2Vec()
+clf = joblib.load('./models/logistic_regression.pkl')
 
 comentario = "Estoy muy triste y no se que hacer"
 # Escribe un comentario triste y largo
@@ -15,3 +17,5 @@ cosine_similarity_beck = w2v.get_cosine_similarity_BECK(comentario_procesado)
 # Obtener la respuesta por item basandose en la similitud de coseno
 results_beck = w2v.get_result_beck(cosine_similarity_beck)
 print("El comentario lleno el inventario BECK de esta manera:", results_beck)
+
+print('Predicción:', clf.predict([results_beck]))
